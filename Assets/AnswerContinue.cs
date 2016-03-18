8fa1bd8ef700b5089b607f515d2c3585d2f4c235
Lucas Gutierres Gomes <lucas.gutierres.gomes@gmail.com>
2016-03-18 11:56:05 -0300

    One of the placeholder images being randomly instantiated on start

---
+++ Assets/AnswerContinue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerContinue : MonoBehaviour {
+
+	void Start () {
+	}
+
+	void Update () {
+		Debug.Log ("Good Job!");	
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	public GameObject[] imgList;
+	public int imgAnswer;
+	public int[] imgOptions;
+	public Transform parent;
+	bool answerExists = true;
+	
+	void Start () {
+		imgList = Resources.LoadAll<GameObject> ("Prefabs");
+		imgAnswer = Random.Range (0, 4);
+		Debug.Log (imgList[imgAnswer]);
+		parent = GameObject.Find("Canvas").transform;
+	}
+	
+	void imgDraw () {
+		GameObject answer = Instantiate (imgList [imgAnswer]);
+		answer.transform.SetParent (parent ,false);
+		answer.transform.position = new Vector3 (0,0,0);
+		answerExists = false;
+	}
+	
+	void Update () {
+		if (answerExists)
+			imgDraw ();
+	}
+}
